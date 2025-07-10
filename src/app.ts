d3fb4855219cc9e07eacd93948ff486d68e84ec8
Rohithossain12 <https://github.com/Rohithossain12>
2025-07-10 17:37:07 +0600

    Project setup complete

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('Welcome to Tour Management System Backend')
+});
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import dotenv from 'dotenv';
+
+
+let server: Server;
+const PORT = 5000;
+dotenv.config();
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uv360.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log("Connected to DB");
+
+        server = app.listen(PORT, () => {
+            console.log(`Server is listening on PORT ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startServer();
+
+
